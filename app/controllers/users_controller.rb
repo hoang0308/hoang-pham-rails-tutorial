a8efaf178032d6eb7e0bcea9eb058d4c17b1dfce
hoang0308 <hoangph030898@gmail.com>
2021-10-20 19:45:25 +0700

    Finish chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+  
+  def new
+    @user = User.new
+  end
+  def create
+    # @user = User.new(params[:user]) # not the final implementation
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user   # ~ redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                  :password_confirmation)
+      # tra ve 1 version params hash voi cac attributes trong permit
+      # va se phat sinh error neu :user attribute loi
+    end
+
 end
