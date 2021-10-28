e5307b16eaad4c9bc77a88be18cb126a8650959e
hoang0308 <hoangph030898@gmail.com>
2021-10-28 09:16:48 +0700

    update chapter7 def age_limit

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,12 +17,10 @@ class User < ApplicationRecord
 
     private
 
-        def age_limit 
-            if self.age
-                age = Date.today.year - self.age.year
-                if(age>100 || age<18)
-                    errors.add :age, "must be between 18 and 100"
-                end
+        def age_limit
+            age = Date.today.year - self.age.year
+            if(age>100 || age<18)
+                errors.add :age, "must be between 18 and 100"
             end
         end
 
