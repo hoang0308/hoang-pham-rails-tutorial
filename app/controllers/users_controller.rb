c76347b287739c6faa6e971baf9e2ae5d949f58a
hoang0308 <hoangph030898@gmail.com>
2021-11-08 14:23:52 +0700

    update chapter11: fix conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,9 @@
 class UsersController < ApplicationController
-<<<<<<< HEAD
-  before_action :logged_in_user, only: [:show, :index, :edit, :update, :destroy]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
-=======
   before_action :logged_in_user, except: [:new, :create]
   before_action :find_user, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
  
->>>>>>> chapter10
   
   def index
     @users = User.paginate page: params[:page]
@@ -34,19 +28,10 @@ class UsersController < ApplicationController
   end
 
   def edit
-<<<<<<< HEAD
-    @user = User.find_by id: params[:id]
-  end
-
-  def update
-    @user = User.find_by id: params[:id]
-    if @user.update(user_params)
-=======
   end
 
   def update
     if @user.update user_params
->>>>>>> chapter10
       flash[:success] = t(".fl_success")
       redirect_to @user
     else
@@ -55,11 +40,7 @@ class UsersController < ApplicationController
   end
 
   def destroy
-<<<<<<< HEAD
-    User.find(params[:id]).destroy
-=======
     @user.destroy
->>>>>>> chapter10
     flash[:success] = t(".flash_delete")
     redirect_to users_url
   end
@@ -78,7 +59,7 @@ class UsersController < ApplicationController
       unless logged_in?
         store_location
         flash[:danger] = t("flash_pl_login")
-        redirect_to login_url unless current_user?(@user)
+        redirect_to login_url
       end
     end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,6 +16,14 @@ class User < ApplicationRecord
     before_save :downcase_email
     before_create :create_activation_digest
 
+    class << self
+        def digest(string)
+            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                        BCrypt::Engine.cost
+            BCrypt::Password.create(string, cost: cost)
+        end
+    end
+
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                     BCrypt::Engine.cost
@@ -51,40 +59,10 @@ class User < ApplicationRecord
         UserMailer.account_activation(self).deliver_now
     end
 
-<<<<<<< HEAD
-    class << self
-        def digest(string)
-            cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                        BCrypt::Engine.cost
-            BCrypt::Password.create(string, cost: cost)
-        end
-    end
-
     def current_user?(current_user) 
         self && self == current_user
     end
-        
-    #return random token
-    def User.new_token
-        SecureRandom.urlsafe_base64
-    end
 
-    def remember
-        self.remember_token = User.new_token
-        update_attribute(:remember_digest, User.digest(remember_token))
-    end
-
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end
-
-    def forget
-        update_attribute(:remember_digest, nil)
-    end
-
-=======
->>>>>>> 90816bb (update chapter11 05/11)
     private
 
         def age_limit
