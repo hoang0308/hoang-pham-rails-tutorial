78a017fc6c65e04c16c0765fb947dbe3ad1e9c52
hoang0308 <hoangph030898@gmail.com>
2021-11-15 11:47:30 +0700

    update chapter12 15/11 v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    attr_accessor :remember_token, :activation_token
+    attr_accessor :remember_token, :activation_token, :reset_token
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     enum genders: [ "Male", "Female", "N/A" ]
     enum status: [:deactive, :active ]
@@ -30,9 +30,6 @@ class User < ApplicationRecord
         end
     end
 
-    #return random token
-   
-
     def remember
         self.remember_token = User.new_token
         update remember_digest: User.digest(remember_token)
@@ -59,6 +56,20 @@ class User < ApplicationRecord
 
     def current_user?(current_user) 
         self && self == current_user
+    end
+
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update reset_digest: User.digest(reset_token),
+            reset_sent_at: Time.zone.now
+    end
+
+    def send_password_reset_mail
+        UserMailer.password_reset(self).deliver_now
+    end
+
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   get 'users/new'
   root 'static_pages#home'
@@ -6,10 +8,11 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'  #, as: 'hoang'   #(thay doi ten path)
   get '/contact', to: 'static_pages#contact'
   get '/sigup', to: 'users#new'
+  get '/users/sendusers', to: 'users#send_user'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/users/sendusers', to: 'users#send_user'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
