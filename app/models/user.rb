6738ff665b30106fbdf8bd3de103ba8e87c30ab2
hoang0308 <hoangph030898@gmail.com>
2021-11-16 16:33:26 +0700

    update chapter14 16/11

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name: "Relationship",
+                                foreign_key: "follower_id",
+                                dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship",
+                                foreign_key: "followed_id",
+                                dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+
+
     attr_accessor :remember_token, :activation_token, :reset_token
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     enum genders: [ "Male", "Female", "N/A" ]
@@ -77,7 +87,19 @@ class User < ApplicationRecord
         # Micropost.where("user_id = ?", id)
         self.microposts
     end
+
+    def follow(other_user)
+        following << other_user
+    end
     
+    def unfollow(other_user)
+        following.delete(other_user)
+    end
+
+    def following?(other_user)
+        following.include?(other_user)
+    end
+
     private
 
         def age_limit
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,37 @@
-User.create!(name: "hoang",
-    gender: "Male",
-    age: "1998-01-01",
-    email: "hoang@gmail.com",
-    password: "123456",
-    password_confirmation: "123456",
-    user_type: 1,
-    status: 1,
-    status_at: Time.zone.now)
-99.times do |n|
-    name = "hoang-#{n+1}"
-    gender = "Male"
-    age = "1998-01-01"
-    email = "hoang#{n+1}@gmail.com"
-    password = "123456"
-    User.create!(name: name,
-    gender: gender,
-    age: "1998-01-01",
-    email: email,
-    password: password,
-    password_confirmation: password,
-    status: 1,
-    status_at: Time.zone.now)
-end
+# User.create!(name: "hoang",
+#     gender: "Male",
+#     age: "1998-01-01",
+#     email: "hoang@gmail.com",
+#     password: "123456",
+#     password_confirmation: "123456",
+#     user_type: 1,
+#     status: 1,
+#     status_at: Time.zone.now)
+# 99.times do |n|
+#     name = "hoang-#{n+1}"
+#     gender = "Male"
+#     age = "1998-01-01"
+#     email = "hoang#{n+1}@gmail.com"
+#     password = "123456"
+#     User.create!(name: name,
+#     gender: gender,
+#     age: "1998-01-01",
+#     email: email,
+#     password: password,
+#     password_confirmation: password,
+#     status: 1,
+#     status_at: Time.zone.now)
+# end
 # Generate microposts for a subset of users.
-users = User.order(:created_at).take(6)
-50.times do |n|
-    content = "content-#{n+1}"
-    users.each { |user| user.microposts.create!(content: content) }
-end
+# users = User.order(:created_at).take(6)
+# 50.times do |n|
+#     content = "content-#{n+1}"
+#     users.each { |user| user.microposts.create!(content: content) }
+# end
 
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
