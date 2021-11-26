ca9316b6b2cbeaef6098d4e9b0c7dc79bc959795
hoang0308 <hoangph030898@gmail.com>
2021-11-26 16:06:25 +0700

    update chapter14 26/11

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,7 +5,7 @@ module UsersHelper
         gravatar_id = Digest::MD5::hexdigest(user.email.downcase) 
         gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"  #lay url anh
         # gravatar_url = "https://vinasupport.com/assets/img/vinasupport_logo.png"  #thay url anh khac
-        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+        image_tag(gravatar_url, alt: user.name, class: "float-left mb-1 ml-1")
     end
         
 end
